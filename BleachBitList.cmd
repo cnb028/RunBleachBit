8436b131c364b1bb70a9e4b64467b9a62b04b184
Christine_Bettencourt <85318035+cnb028@users.noreply.github.com>
2024-11-13 17:11:24 -0600

    Update to BleachBitList.cmd
    Updated BleachbitList.cmd to remove Action One Test results, as they have been moved to README.md. Also added Pause command.

--- BleachBitList.cmd
+++ BleachBitList.cmd
@@ -3,7 +3,6 @@
 ::Script Language: Command Prompt
 ::Created: 11/8/2024
 ::Last Edited: 11/12/2024
-::Action One Test Results: Pulls List but gets Runtime Error
 
 @echo off
 
@@ -19,3 +18,5 @@ cd "C:\Program Files (x86)\BleachBit"
 ::Execute Bleachbit with the specificed operations
 
 bleachbit_console.exe -l
+
+pause
