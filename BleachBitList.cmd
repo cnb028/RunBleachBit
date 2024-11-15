ce4f8a7fcb1925890b0f9c105f2fa915b5e42276
Christine_Bettencourt <85318035+cnb028@users.noreply.github.com>
2024-11-15 12:22:08 -0600

    Update BleachBitList.cmd

--- BleachBitList.cmd
+++ BleachBitList.cmd
@@ -2,15 +2,19 @@
 ::Creator: Christine Bettencourt
 ::Script Language: Command Prompt
 ::Created: 11/8/2024
-::Last Edited: 11/12/2024
+::Last Edited: 11/14/2024
 
 @echo off
+export LANG=C. UFT-8
+
+::Check Console Encoding
+chcp
 
 ::Change Console Encoding
 chcp 65001
 
 ::Set Pythonioencoding Environment Variable
-set Pythonioencoding=UTF-8
+::set Pythonioencoding=UTF-8
 
 ::Change to the Bleachbit installation directory
 cd "C:\Program Files (x86)\BleachBit"
